d0d74152fbf10931f2ecde089d3066834d4cc802
yanabirukova <y.birukova@gmail.com>
2022-10-31 20:54:48 +0300

    xvv

--- app/src/main/java/com/example/myfirstapptms/HelloWorld.java
+++ app/src/main/java/com/example/myfirstapptms/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example.myfirstapptms;
 
 public class HelloWorld {
     public static void main (String[] args) {
-        System.out.println("Hello, World!asd");
+        System.out.println("Hello, World!");
     }
 }
