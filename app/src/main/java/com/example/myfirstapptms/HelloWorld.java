82de73e9f637a44df2587b7be6df652e86abd0d5
yanabirukova <y.birukova@gmail.com>
2022-11-02 21:22:15 +0300

    hello android

--- app/src/main/java/com/example/myfirstapptms/HelloWorld.java
+++ app/src/main/java/com/example/myfirstapptms/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example.myfirstapptms;
 
 public class HelloWorld {
     public static void main (String[] args) {
-        System.out.println("Hello, World!mjgk");
+        System.out.println("Hello,Android");
     }
 }
